d653e9cc692a0f1bd8d0e297aad8cd871fde61da
Hirepan Palomares <hirepan.49@gmail.com>
2023-06-14 23:50:24 +0200

    commit before changing to main branch to erase the res_files

--- Shynt/api_py/Mesh/mesh_info.py
+++ Shynt/api_py/Mesh/mesh_info.py
@@ -1,145 +1,154 @@
-from Shynt.api_py.Geometry.utilities_geometry import get_equal_nodes, get_surface_equivalence
+from Shynt.api_py.Geometry.utilities_geometry import get_equal_nodes
+from Shynt.api_py.Geometry.utilities_geometry import get_surface_equivalence
+from Shynt.api_py.Geometry.surfaces import InfiniteHexagonalCylinderXtype
+from Shynt.api_py.Geometry.surfaces import InfiniteHexagonalCylinderYtype
+from Shynt.api_py.Geometry.surfaces import InfiniteSquareCylinderZ
+
 from Shynt.api_py.materials import Material
 
 
 
 class MeshInfo:
 
-    def __init__(self, coarse_nodes, fine_nodes, coarse_nodes_map):
-        self.__coarse_nodes = coarse_nodes
-        self.__fine_nodes = fine_nodes
-        self.coarse_nodes_map = coarse_nodes_map
-        # ------------------------------------------------------------
-        self.coarse_order = list(coarse_nodes.keys())
-        self.all_regions_order = []
-        self.all_surfaces_order = []
-        self.all_regions_vol = {}
-        self.all_surfaces_area = {}
-        self.region_coarse_rel = {}
-        self.coarse_region_rel = {}
-        self.coarse_surface_rel = {}
-        self.equal_nodes_rel = {}
-        self.equivalence_region_rel = {}
-        self.equivalence_surface_rel = {}
-        self.region_type_rel = {}
-        self.region_type_rel_switched = {}
-        self.equal_nodes = {}
-        self.type_system = ""
+  def __init__(self, model_cell):
+    self.__coarse_nodes = model_cell.global_mesh.coarse_nodes
+    self.__fine_nodes = model_cell.global_mesh.fine_nodes
+    self.coarse_nodes_map = model_cell.local_mesh.coarse_nodes_map
+    # ------------------------------------------------------------
+    self.coarse_order = list(self.__coarse_nodes.keys())
+    self.all_regions_order = []
+    self.all_surfaces_order = []
+    self.all_regions_vol = {}
+    self.all_surfaces_area = {}
+    self.region_coarse_rel = {}
+    self.coarse_region_rel = {}
+    self.coarse_surface_rel = {}
+    self.equal_nodes_rel = {}
+    self.equivalence_region_rel = {}
+    self.equivalence_surface_rel = {}
+    self.region_type_rel = {}
+    self.region_type_rel_switched = {}
+    self.equal_nodes = {}
+    self.type_system = ""
+    if isinstance(model_cell.region.surface, InfiniteSquareCylinderZ):
+      self.type_system = "square"
+    elif isinstance(model_cell.region.surface, InfiniteHexagonalCylinderXtype):
+      self.type_system = "hexagonal"
+    elif isinstance(model_cell.region.surface, InfiniteHexagonalCylinderYtype):
+      self.type_system = "hexagonal"
 
-        self.__get_volume_and_areas()
-        self.__get_coarse_to_fine_rel_info()
-        self.__get_region_content_relation()
-        self.__equal_nodes()
-        self.__equal_regions()
-        self.__equal_surfaces()
-        db = True
+    self.__get_volume_and_areas()
+    self.__get_coarse_to_fine_rel_info()
+    self.__get_region_content_relation()
+    self.__equal_nodes()
+    self.__equal_regions()
+    self.__equal_surfaces()
+    db = True
 
-    def __get_volume_and_areas(self):
-        for n_id in self.__coarse_nodes.keys():
-            
-            regions = self.__coarse_nodes[n_id].fine_nodes_ids
-            surfaces = self.__coarse_nodes[n_id].surface_ids
-            # print(surfaces)
-            vols = self.__coarse_nodes[n_id].fine_nodes_volume
-            areas = self.__coarse_nodes[n_id].surface_areas
-            # print_ = [print(key, value) for key,value in self.__coarse_nodes[n_id].geometry_info["surfaces_for_detectors"]["boundary_guide"].items()]
+  def __get_volume_and_areas(self):
+    for n_id in self.__coarse_nodes.keys():
+      regions = self.__coarse_nodes[n_id].fine_nodes_ids
+      surfaces = self.__coarse_nodes[n_id].surface_ids
+      # print(surfaces)
+      vols = self.__coarse_nodes[n_id].fine_nodes_volume
+      areas = self.__coarse_nodes[n_id].surface_areas
+      # print_ = [print(key, value) for key,value in self.__coarse_nodes[n_id].geometry_info["surfaces_for_detectors"]["boundary_guide"].items()]
 
-            self.all_regions_vol.update(vols)
-            self.all_surfaces_area.update(areas)
-            self.all_regions_order += regions
-            self.all_surfaces_order += surfaces
+      self.all_regions_vol.update(vols)
+      self.all_surfaces_area.update(areas)
+      self.all_regions_order += regions
+      self.all_surfaces_order += surfaces
 
-    def __get_coarse_to_fine_rel_info(self):
-        for n_id in self.__coarse_nodes.keys():
-            
-            regions = self.__coarse_nodes[n_id].fine_nodes_ids
-            surfaces = self.__coarse_nodes[n_id].surface_ids
+  def __get_coarse_to_fine_rel_info(self):
+    for n_id in self.__coarse_nodes.keys():
+      regions = self.__coarse_nodes[n_id].fine_nodes_ids
+      surfaces = self.__coarse_nodes[n_id].surface_ids
 
-            self.coarse_region_rel[n_id] = regions
-            self.coarse_surface_rel[n_id] = surfaces
+      self.coarse_region_rel[n_id] = regions
+      self.coarse_surface_rel[n_id] = surfaces
 
-    def __get_region_content_relation(self):
-        """
-            This method is to generate a relation of what regions 
-            have what material and what is the region of the material
+  def __get_region_content_relation(self):
+    """
+      This method is to generate a relation of what regions 
+      have what material and what is the region of the material
 
-            This is used to know which regions correspond to each other
-            between two coarse nodes that are of the same type
-            
-        """
-        for n_id, coarse_node in self.__coarse_nodes.items():
-            self.region_type_rel[n_id] = {
-                
-            }
-            self.region_type_rel_switched[n_id] = {}
-            for r_id, fine_node  in self.__fine_nodes[n_id].items():
-                self.region_coarse_rel[r_id] = n_id
-                region_cell = fine_node.cell
-                material_name = region_cell.content.name
-                if material_name == "void": continue
-                self.region_type_rel[n_id][material_name + f"_{region_cell.id}"] = r_id
-                self.region_type_rel_switched[n_id][r_id] = material_name  + f"_{region_cell.id}"
-                
-      
-
-    def __equal_nodes(self):
-        """
-            Method to get all the nodes that are of the same type
-            and also the variable self.equal_nodes_rel
-
-            
-        """
-        self.equal_nodes = get_equal_nodes(self.__coarse_nodes, self.__fine_nodes)
-        # print(self.equal_nodes)
-
-        # Turn equal_nodes into a list
-        self.equal_nodes = [bin_ for bin_ in self.equal_nodes.values()]
-        for bin_ in self.equal_nodes:
-            head = bin_[0]
-            for node_id in bin_:
-                self.equal_nodes_rel[node_id] = head
+      This is used to know which regions correspond to each other
+      between two coarse nodes that are of the same type
+        
+    """
+    for n_id, coarse_node in self.__coarse_nodes.items():
+      self.region_type_rel[n_id] = {
+          
+      }
+      self.region_type_rel_switched[n_id] = {}
+      for r_id, fine_node  in self.__fine_nodes[n_id].items():
+        self.region_coarse_rel[r_id] = n_id
+        region_cell = fine_node.cell
+        material_name = region_cell.content.name
+        if material_name == "void": continue
+        self.region_type_rel[n_id][material_name + f"_{region_cell.id}"] = r_id
+        self.region_type_rel_switched[n_id][r_id] = material_name  + f"_{region_cell.id}"
+              
     
 
-    def __equal_regions(self):
-        """
-            The criteria for which a region is equal to other
-            is based on  the order of the regions since the regions 
-            are declared in the same order
+  def __equal_nodes(self):
+      """
+          Method to get all the nodes that are of the same type
+          and also the variable self.equal_nodes_rel
+
+          
+      """
+      self.equal_nodes = get_equal_nodes(self.__coarse_nodes, self.__fine_nodes)
+      # print(self.equal_nodes)
+
+      # Turn equal_nodes into a list
+      self.equal_nodes = [bin_ for bin_ in self.equal_nodes.values()]
+      for bin_ in self.equal_nodes:
+          head = bin_[0]
+          for node_id in bin_:
+              self.equal_nodes_rel[node_id] = head
+  
+
+  def __equal_regions(self):
+      """
+          The criteria for which a region is equal to other
+          is based on  the order of the regions since the regions 
+          are declared in the same order
 
 
-        """
-        for n_id, n_eq_id in self.equal_nodes_rel.items():
-            type_regs = list(self.region_type_rel_switched[n_eq_id].keys())
-            for r, reg_id in enumerate(self.coarse_region_rel[n_id]):
-                reg_eq = type_regs[r]
-                self.equivalence_region_rel[reg_id] = reg_eq
-        
+      """
+      for n_id, n_eq_id in self.equal_nodes_rel.items():
+          type_regs = list(self.region_type_rel_switched[n_eq_id].keys())
+          for r, reg_id in enumerate(self.coarse_region_rel[n_id]):
+              reg_eq = type_regs[r]
+              self.equivalence_region_rel[reg_id] = reg_eq
+      
 
 
-    def __equal_surfaces(self):
-        """
-            The criteria for which a surface is equal to other
-            is based on  the order of the surfaces since the surfaces 
-            are declared in the same order
+  def __equal_surfaces(self):
+      """
+          The criteria for which a surface is equal to other
+          is based on  the order of the surfaces since the surfaces 
+          are declared in the same order
 
 
-        """
-        for n_id, n_eq_id in self.equal_nodes_rel.items():
-            node_ = self.__coarse_nodes[n_id]
-            node_eq = self.__coarse_nodes[n_eq_id]
-            surface_rel = get_surface_equivalence(node_, node_eq)
-            # print(":"*75)
-            # print(f"node = {n_id}\t node_equal = {n_eq_id}")
-            # print("surface relation: ")
-            # print(surface_rel)
-            # print_surf_rel = [print(key,value)  for key,value in surface_rel.items()]
-            # print("surface guide:")
-            # print_guide = [print(key,value) for key,value in node_.geometry_info["surfaces_for_detectors"]["boundary_guide"].items()]
-            # print("surface areas:")
-            # print_areas = [print(key,value) for key,value in node_.geometry_info["surfaces_for_detectors"]["boundary_surfaces_areas"].items()]
+      """
+      for n_id, n_eq_id in self.equal_nodes_rel.items():
+          node_ = self.__coarse_nodes[n_id]
+          node_eq = self.__coarse_nodes[n_eq_id]
+          surface_rel = get_surface_equivalence(node_, node_eq)
+          # print(":"*75)
+          # print(f"node = {n_id}\t node_equal = {n_eq_id}")
+          # print("surface relation: ")
+          # print(surface_rel)
+          # print_surf_rel = [print(key,value)  for key,value in surface_rel.items()]
+          # print("surface guide:")
+          # print_guide = [print(key,value) for key,value in node_.geometry_info["surfaces_for_detectors"]["boundary_guide"].items()]
+          # print("surface areas:")
+          # print_areas = [print(key,value) for key,value in node_.geometry_info["surfaces_for_detectors"]["boundary_surfaces_areas"].items()]
 
-            self.equivalence_surface_rel.update(surface_rel)
-        
+          self.equivalence_surface_rel.update(surface_rel)
+      
 
 
 
--- Shynt/api_py/main/run.py
+++ Shynt/api_py/main/run.py
@@ -1,69 +1,41 @@
 
 from Shynt.api_py.Mesh.mesh_info import MeshInfo
-from Shynt.api_py.Geometry.universes import HexagonalLatticeTypeX, SquareLattice
 from Shynt.api_py.Postprocess.write_output import OutputFile
 from Shynt.api_py.Serpent.serpent_runners import run_detector_files, run_xs_files
-from Shynt.api_py.Probabilities.get_probabilities_system import get_probabilities, check_reciprocity
+from Shynt.api_py.Probabilities.get_probabilities_system import get_probabilities
 from Shynt.api_py.CrossSections.get_xs_system import get_xs_data
 from Shynt.api_py.ResponseMatrix.iterations import solveKeff, solveKeff_byGroup
 from Shynt.api_py.Serpent.file_generator import generate_serpent_files
 
-from Shynt.api_py.Geometry.surfaces import InfiniteHexagonalCylinderXtype, InfiniteHexagonalCylinderYtype, InfiniteSquareCylinderZ, reset_surface_counter
 from Shynt.api_py.Geometry.cells import reset_cell_counter
-from Shynt.api_py.Probabilities.probability_writer import write_excel
-
-import numpy as np
-
+from Shynt.api_py.Geometry.surfaces import reset_surface_counter
 
 
 def run(root, server=True, name_out="", serp_dir="serpent_files"):
     
-  # Extracting cells from root model
+  # Extracting model cell root model --------------------------------------------------
   model_cell = root.model_cell
 
-  energy_g = root.energy_grid.energy_groups
-  coarse_nodes = model_cell.global_mesh.coarse_nodes
-  coarse_nodes_map = model_cell.global_mesh.coarse_nodes_map
-  fine_nodes = model_cell.local_mesh.fine_nodes
-
-  # Generating mesh info
-  mesh_info = MeshInfo(coarse_nodes, fine_nodes, coarse_nodes_map)
-  if isinstance(model_cell.region.surface, InfiniteSquareCylinderZ):
-    mesh_info.type_system = "square"
-  elif isinstance(model_cell.region.surface, InfiniteHexagonalCylinderXtype):
-    mesh_info.type_system = "hexagonal"
-  elif isinstance(model_cell.region.surface, InfiniteHexagonalCylinderYtype):
-    mesh_info.type_system = "hexagonal"
+  # Generating mesh info --------------------------------------------------------------
+  mesh_info = MeshInfo(model_cell)
   
-  # ? Checking mesh info --------------------------------------------------------------------------
-  # print(mesh_info.equal_nodes_rel)
-  # print(mesh_info.coarse_region_rel)
-  # print(mesh_info.region_type_rel_switched)
-  # ? ---------------------------------------------------------------------------------------------
-
-
-
-  # Generate local problems files, and file for cross section generation
+  # Generate local problems files, and file for cross section generation --------------
   det_inputs, xs_inputs = generate_serpent_files(root, mesh_info.equal_nodes, serp_dir=serp_dir)
-  # Running generated files
+  
+  # Running generated files -----------------------------------------------------------
   run_detector_files(det_inputs)
   run_xs_files(xs_inputs)
 
-  # raise SystemExit
-  # get XS and probabilities
+  # get XS and probabilities ----------------------------------------------------------
   xs = get_xs_data(mesh_info, root, xs_inputs)
   probabilities, prob_sigma = get_probabilities(det_inputs, mesh_info, root, xs)
-  
-  
-  
+
   reset_surface_counter()
   reset_cell_counter()
   
-  # Run detector files 
-  # Solve iterative method ----------------------------------------------------
+  # Solve iterative method ------------------------------------------------------------
   # solution = solveKeff(root, xs, probabilities, mesh_info, prob_sigma)
   solution = solveKeff_byGroup(root, xs, probabilities, mesh_info, prob_sigma)
-
 
   keff = solution["keff"]
   phi = solution["phi"]
